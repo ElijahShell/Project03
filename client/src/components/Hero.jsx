d55d1f881896f35a6f2f2336ebc62c0d8935271f
Elijah <kamokane06@gmail.com>
2026-03-31 17:55:55 -0400

    Creating Hero

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import "../styles/hero.css";
+
+export default function Hero() {
+    return(
+        <header className="hero">
+            <div className="overlay"></div>
+            <div className="hero-content">
+                <h1>
+                   
+                </h1>
+            </div>
+        </header>
+    )
+}
