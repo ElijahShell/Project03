2b2e7f8609792820134a098ae317c9663d490567
Elijah <kamokane06@gmail.com>
2026-03-31 17:13:54 -0400

    Fixed LocalHost issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
+import Navbar from "./components/Navbar";
+
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>Welcome to the Restaurant App!</h1>
-    </div>
+    <>
+    <Navbar />
+    </>
   )
 }
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+export default function Navbar() {
+    return(
+        <nav className="navbar">
+            <h2 className="logo">Wolfe's Tattoo Gallery</h2>
+        
+            <ul className="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Aftercare</a></li>
+            </ul>
+        </nav>
+    );
+
+}
